88a4c664574ec6537c7db6e8ae27e9b2be8ee1af
Elena Parchevskaya <elena.lemoon@gmail.com>
2022-05-02 22:55:07 +0300

    fix application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,9 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 import "controllers"
 
-import { Application } from "@hotwired/stimulus"
-
-const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
