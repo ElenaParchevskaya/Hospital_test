a13ead2c1dd2797c69e94d640c423c975d89a707
Elena Parchevskaya <elena.lemoon@gmail.com>
2022-05-02 22:35:19 +0300

    fix javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,11 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
+
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
